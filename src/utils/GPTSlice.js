7e4f17f441a5b8cc87d53033ba290b2a2bbc7b9c
Soumak Dutta <Soumak.Dutta@dnow.com>
2024-02-12 13:15:02 +0530

    Final commit

--- src/utils/GPTSlice.js
+++ src/utils/GPTSlice.js
@@ -3,15 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 const GPTSlice = createSlice({
     name: "gpt",
     initialState : {
-        showGPTSearch : false
+        showGPTSearch : false,
+        movieResults: null,
+        movieNames: null,
     },
     reducers : {
         toggleGPTSearchView : (state) =>  {
             state.showGPTSearch = !state.showGPTSearch;
-        }
+        },
+        addGptMovieResult: (state, action) => {
+            const { movieNames, movieResults } = action.payload;
+            state.movieNames = movieNames;
+            state.movieResults = movieResults;
+          },
     }
 
 })
-export const {toggleGPTSearchView} = GPTSlice.actions;
+export const {toggleGPTSearchView, addGptMovieResult} = GPTSlice.actions;
 
 export default GPTSlice.reducer;
